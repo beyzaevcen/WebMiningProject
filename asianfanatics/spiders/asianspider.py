d9d0448b125e3f5aab61a2112bc7a480bbb69814
Beyza Evcen <beyzaevcen37@icloud.com>
2024-05-05 17:02:27 +0300

    All datas transfered to csv

--- asianfanatics/spiders/asianspider.py
+++ asianfanatics/spiders/asianspider.py
@@ -4,6 +4,7 @@ class AsianspiderSpider(scrapy.Spider):
     name = "asianspider"
     allowed_domains = ["asyafanatiklerim.com"]
     start_urls = ["https://asyafanatiklerim.com/tur/macera","https://asyafanatiklerim.com/tur/romantik","https://asyafanatiklerim.com/tur/dram","https://asyafanatiklerim.com/tur/aile","https://asyafanatiklerim.com/tur/aksiyon","https://asyafanatiklerim.com/tur/fantastik","https://asyafanatiklerim.com/tur/genclik","https://asyafanatiklerim.com/tur/gerilim","https://asyafanatiklerim.com/tur/gizem","https://asyafanatiklerim.com/tur/hukuk","https://asyafanatiklerim.com/tur/komedi","https://asyafanatiklerim.com/tur/medikal","https://asyafanatiklerim.com/tur/polisiye","https://asyafanatiklerim.com/tur/tarih"]
+    
 
     def parse(self, response):
         category = response.css('div.content')
@@ -26,3 +27,4 @@ class AsianspiderSpider(scrapy.Spider):
 
         if next_page:  # next_page boş değilse, yani None değilse
             yield response.follow(next_page, callback=self.parse)
+
