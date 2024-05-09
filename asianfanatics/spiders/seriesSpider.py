cfebb124723b092a9b0afad91d53208aeeb7112e
zehraozturkk <fzehraozturk34@gmail.com>
2024-05-09 12:39:25 +0300

    We started scraping the information of the series in the url list.

--- asianfanatics/spiders/seriesSpider.py
+++ asianfanatics/spiders/seriesSpider.py
@@ -1,30 +1,33 @@
 import scrapy
-
+import pandas as pd
 
 class SeriesspiderSpider(scrapy.Spider):
     name = "seriesSpider"
     allowed_domains = ["asyafanatiklerim.com"]
-    start_urls = ["https://asyafanatiklerim.com/dizi/the-three-musketeers/"]
+
+    def start_requests(self):
+        df = pd.read_csv("C:\\Users\\fzehr\\OneDrive\\Belgeler\\GitHub\\WebMiningProject\\asianfanatics\\alldatas.csv")
+        url_list = df["url"].str.strip()
+
+        for url in url_list:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-
         content = response.css("div.content")
-        #episode_list = content.css("ul.episodios li")
         cast_div =  content.css("div#cast div.persons")
         persons = cast_div.css("div.persons div.person")
 
-        cast_list= []
+        cast_list = []
         for person in persons:
             actor_dict = {
                 "name": person.css("div.data div.name a::text").get(),
                 "caracter": person.css("div.data div.caracter::text").get() 
             }
-
             cast_list.append(actor_dict)
-        yield{
+
+        yield {
             "name": content.css("div.data h1::text").get(),
             "full_date": content.css("span.date::text").get(),
             "total_episode": content.css('div.custom_fields b.variante:contains("Bölümler") + span.valor::text').get(),
             "cast": cast_list
         }
-
